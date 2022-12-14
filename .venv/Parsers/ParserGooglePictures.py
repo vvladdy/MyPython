3438663300c6cf0dd6f091b540394943fbf373cb
vvladdy <yablonvl@icloud.com>
2022-12-14 08:45:53 +0200

    Parsfoto from Google icrawler

---
+++ .venv/Parsers/ParserGooglePictures.py
@@ -0,0 +1,17 @@
+from icrawler.builtin import GoogleImageCrawler
+
+save_image_dir = r'D:\MyPythonFolder\MyPython\.venv\Dif_files\ImagesGoogle'
+
+google_crawler = GoogleImageCrawler(
+    storage={'root_dir': save_image_dir}
+)
+
+name_request = 'background movies avatar'
+quantity_image = 3
+
+filters = dict(
+    size='large'
+)
+
+google_crawler.crawl(keyword=name_request, filters=filters,
+                     max_num=quantity_image,  min_size=(400, 200))
